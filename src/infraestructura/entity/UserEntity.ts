c826261f1a917adef94686ea2eae92e596a7d752
DannaGA66 <dannaacev@hotmail.com>
2025-09-13 23:53:28 -0500

    user and person entities

---
+++ src/infraestructura/entity/UserEntity.ts
@@ -0,0 +1,28 @@
+import { Role } from "@/domain/user/Role";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "user" })
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id_user!: number;
+
+    @Column({ type: "varchar", length: 255 })
+    name_user!: string;
+
+    @Column({ type: "varchar", length: 255, unique: true })
+    email_user!: string;
+
+    @Column({ type: "varchar", length: 255 })
+    password_user!: string;
+
+    @Column({ type: "boolean" })
+    active_user!: boolean;
+
+    @Column({
+        type: "enum",
+        enum: Role,
+        default: Role.USER
+    })
+    role_user!: Role;
+
+}
